3f93e6d1c436eef707f580e23ad73bc0f87fa2f5
MannurM <zmannur@gmail.com>
2022-04-19 07:21:52 +0400

    19/04/2022

--- create_user_sert.py
+++ create_user_sert.py
@@ -10,62 +10,11 @@ import modules
 
 
 # конфигурация
+from FDataBase import FDataBase
+
 DATABASE = 'pr_ot.db'
 DEBUG = True
 SECRET_KEY = 'fdgfh78@#5?>gfhf89dx,v06k'
-
-
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.update(dict(DATABASE=os.path.join(app.root_path, 'pr_ot.db')))
-
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'index'
-login_manager.login_message = "Авторизуйтесь для доступа к закрытым страницам"
-login_manager.login_message_category = "success"
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return UserLogin().fromDB(user_id, dbase)
-
-
-def connect_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def create_db():
-    """Вспомогательная функция для создания таблиц БД"""
-    db = connect_db()
-    with app.open_resource('sq_db.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-    db.close()
-
-
-def get_db():
-    """Соединение с БД, если оно еще не установлено"""
-    if not hasattr(g, 'link_db'):
-        g.link_db = connect_db()
-    return g.link_db
-
-
-@app.before_request
-def before_request():
-    """Установление соединения с БД перед выполнением запроса"""
-    global dbase
-    db = get_db()
-    dbase = FDataBase(db)
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Закрываем соединение с БД, если оно было установлено"""
-    if hasattr(g, 'link_db'):
-        g.link_db.close()
 
 
 def past_in_templates(data_sert):
@@ -77,7 +26,7 @@ def past_in_templates(data_sert):
     dict_label_user = create_dict_label_user()
     for label in dict_label_user.keys():
         if label in data_sert.keys():
-            text = data_sert['label']
+            text = data_sert[label]
             coordinat = dict_label_user[label]['coordinat']
             font = dict_label_user[label]['font']
             fill = dict_label_user[label]['fill']
@@ -175,7 +124,15 @@ def create_dict_label_user():
     return dict_label_user
 
 
+def connect_db():
+    conn = sqlite3.connect('pr_ot.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
 def create_sert(user_id):
+    db = connect_db()
+    dbase = FDataBase(db)
     theme, count_prob, status_exzam, data_exzam = dbase.getStatus_exzam(user_id=user_id)
     theme2, course_hourses = dbase.read_for_sert()
     data_org = dbase.read_organization()
